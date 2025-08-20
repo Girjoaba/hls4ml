78270cef553fbbb8658c79c2cdf602c07eb45bab
Girjoaba <agirjoaba@student.ethz.ch>
2025-08-20 14:47:31 +0200

    init writer dynamatic

--- hls4ml/templates/dynamatic/firmware/myproject.c
+++ hls4ml/templates/dynamatic/firmware/myproject.c
@@ -19,108 +19,26 @@ typedef int default_t;
 
 // hls-fpga-machine-learning load weights
 
+
 // ****************************************
 // NETWORK INSTANTIATION
 // ****************************************
-pub fn myproject_architecture(
+void myproject(
     // hls-fpga-machine-learning architecture arguments
-    ) ->
-    // hls-fpga-machine-learning output 
-    {
+    ) {
 
     // hls-fpga-machine-learning insert layers
 }
-void jet_tagging2(
-        default_t input0, default_t input1, default_t input2, default_t input3, default_t input4, default_t input5, default_t input6, default_t input7, default_t input8, default_t input9, default_t input10, default_t input11, default_t input12, default_t input13, default_t input14, default_t input15, 
-        default_t out0[OUT_L0], 
-        default_t out1[OUT_L1], 
-        default_t out2[OUT_L2], 
-        default_t out3[OUT_L3]) {
 
-    default_t tmp_input[IN_L0] = {
-        input0, input1, input2, input3, input4, input5, input6, input7, input8, input9, input10, input11, input12, input13, input14, input15
-    };
-
-    /* ----------- Layer 0 ------------ */
-    dense_accum_t acc0;
-    default_t tmp_relu0 = 0;
-    #pragma clang loop unroll_count(UNROLL_FACTOR)
-    for (int j = 0; j < OUT_L0; j++) {                       
-        acc0 = 0;       
-        acc0 = (dense_accum_t)(b0[j] << (FRAC_DEFAULT));
-        #pragma clang loop unroll_count(UNROLL_FACTOR)
-        for (int i = 0; i < IN_L0; i++) {    
-            acc0 += tmp_input[i] * w0[j*IN_L0 + i];           
-        }
-        /* TRUNCATE */                                       
-        acc0 = acc0 >> (FRAC_DEFAULT);             
-        tmp_relu0 = (default_t)acc0;                     
-        /* RELU ACTIVATION */                                
-        out0[j] = tmp_relu0 > 0 ? tmp_relu0 : 0;                   
-    }
-
-    /* ----------- Layer 1 ------------ */
-    dense_accum_t acc1;
-    default_t tmp_relu1 = 0;
-    // #pragma clang loop unroll_count(UNROLL_FACTOR)
-    for (int j = 0; j < OUT_L1; j++) {                            
-        acc1 = (dense_accum_t)(b1[j] << (FRAC_DEFAULT));
-        // #pragma clang loop unroll_count(UNROLL_FACTOR)
-        for (int i = 0; i < IN_L1; i++) {    
-            acc1 += out0[i] * w1[j*IN_L1 + i];           
-        }                  
-        /* TRUNCATE */                                       
-        acc1 = acc1 >> (FRAC_DEFAULT);             
-        tmp_relu1 = (default_t)acc1;                     
-        /* RELU ACTIVATION */                                
-        out1[j] = tmp_relu1 > 0 ? tmp_relu1 : 0;                   
-    }
-
-    /* ----------- Layer 2 ------------ */
-    dense_accum_t acc2;
-    default_t tmp_relu2 = 0;
-    // #pragma clang loop unroll_count(UNROLL_FACTOR)
-    for (int j = 0; j < OUT_L2; j++) {                            
-        acc2 = (dense_accum_t)(b2[j] << (FRAC_DEFAULT));
-        // #pragma clang loop unroll_count(UNROLL_FACTOR)
-        for (int i = 0; i < IN_L2; i++) {    
-            acc2 += out2[i] * w2[j*IN_L2 + i];           
-        }                  
-        /* TRUNCATE */                                       
-        acc2 = acc2 >> (FRAC_DEFAULT);             
-        tmp_relu2 = (default_t)acc2;                     
-        /* RELU ACTIVATION */                                
-        out2[j] = tmp_relu2 > 0 ? tmp_relu2 : 0;                   
-    }
-
-    /* ----------- Layer 3 ------------ */
-    // #pragma clang loop unroll_count(UNROLL_FACTOR)
-    dense_accum_t acc3;
-    default_t tmp_relu3 = 0;
-    for (int j = 0; j < OUT_L3; j++) {                        
-        acc3 = (dense_accum_t)(b3[j] << (FRAC_DEFAULT));
-        // #pragma clang loop unroll_count(UNROLL_FACTOR)
-        for (int i = 0; i < IN_L3; i++) {    
-            acc3 += out2[i] * w3[j*IN_L3 + i];           
-        }                  
-        /* TRUNCATE */
-        acc3 = acc3 >> (FRAC_DEFAULT);
-        tmp_relu3 = (default_t)acc3;
-        /* RELU ACTIVATION */
-        out3[j] = tmp_relu3 > 0 ? tmp_relu3 : 0;                   
-    }
-}
-
-
-
-pub fn myproject(
-    // hls-fpga-machine-learning top function input
-    )-> 
-    // hls-fpga-machine-learning top function output
-    {
-
+int main(void) {
+    srand(13);
     
-    myproject_architecture(
-        // hls-fpga-machine-learning call inlined weights
-    )
+    // hls-fpga-machine-learning input init
+
+    CALL_KERNEL(
+        myproject,
+        // hls-fpga-machine-learning input kernel
+    );
+    return 0;
+    
 }
--- hls4ml/writer/dynamatic_writer.py
+++ hls4ml/writer/dynamatic_writer.py
@@ -51,100 +51,103 @@ class DynamaticWriter(Writer):
             if 'myproject' in line:
                 newline = line.replace('myproject', model.config.get_project_name())
 
-            elif '// hls-fpga-machine-learning imports' in line:
-                newline = line
-                seen_libs = []
-                for layer in layers:
-                    lib = layer.get_attr('func_call').split('::', 1)[0]
-                    if lib and lib not in seen_libs:
-                        seen_libs.append(lib)
-                        newline += f'import nnet_utils.{lib};\n'
+            # elif '// hls-fpga-machine-learning imports' in line:
+            #     newline = line
+            #     seen_libs = []
+            #     for layer in layers:
+            #         lib = layer.get_attr('func_call').split('::', 1)[0]
+            #         if lib and lib not in seen_libs:
+            #             seen_libs.append(lib)
+            #             newline += f'import nnet_utils.{lib};\n'
 
             elif '// hls-fpga-machine-learning insert dimensions' in line:
                 newline = line
-                for layer in layers:
+                for i, layer in enumerate(layers):
                     if layer.get_attr("write_dims"):
-                        newline += f'const {layer.get_attr("out_dim_key")} = u32:{layer.get_attr("out_dim_val")};\n'
+                    #     if layer.class_name == 'Input':
+                    #         newline += f'#define INPUT_SIZE {layer.get_attr("out_dim_val")}\n'
+                    #     else:
+                    #         newline += f'#define {layer.get_attr("in_dim_key")} {layer.get_attr("in_dim_val")}\n'
+                        newline += f'#define {layer.get_attr("out_dim_key")} {layer.get_attr("out_dim_val")}\n'
 
             elif '// hls-fpga-machine-learning architecture arguments' in line:
                 newline = ''
-                weighted_layers_count = 0
+                func_layers_count = 0
                 for i, layer in enumerate(layers):
                     if layer.class_name == 'Input':
-                        newline += indent + f'x: {layer.get_attr("out_type")}[{layer.get_attr("out_dim_key")}],\n'
-                    elif layer.get_attr("write_weights"):
-                        newline += indent + f'w{i}: {layer.get_attr("in_type")}[{layer.get_attr("in_dim_key")}][{layer.get_attr("out_dim_key")}],\n'
-                        newline += indent + f'b{i}: {layer.get_attr("in_type")}[{layer.get_attr("out_dim_key")}]'
-                        if weighted_layers_count < len([layer for layer in layers if layer.get_attr("write_weights")]) - 1:
+                        newline += indent + f'default_t input[{layer.get_attr("out_dim_key")}], \n'
+                    elif layer.get_attr('write_func'):
+                        newline += indent + f'default_t out{i}[{layer.get_attr("out_dim_key")}]'
+                        if func_layers_count < len([layer for layer in layers if layer.get_attr("write_func")]) - 1:
                             newline += ',\n'
-                            weighted_layers_count += 1
+                            func_layers_count += 1
                         else:
                             newline += '\n'
 
-            elif '// hls-fpga-machine-learning output ' in line:
-                last_layer_type = layers[-1].get_attr("out_type")
-                last_layer_dim_key = layers[-1].get_attr("out_dim_key")
-                newline = indent + f'{last_layer_type}[{last_layer_dim_key}]\n'
+            # elif '// hls-fpga-machine-learning insert layers' in line:
+            #     newline = line
+            #     prev_var = 'x'
+            #     for i, layer in enumerate(layers):
+            #         if layer.get_attr('write_func'):
+            #             if layer.get_attr('write_weights'):
+            #                 newline += indent + f'let z{i} = {layer.get_attr("func_call")}({prev_var}, w{i}, b{i});\n'
+            #                 prev_var = f'z{i}'
+            #             else:
+            #                 newline += indent + f'let z{i} = {layer.get_attr("func_call")}({prev_var});\n'
+            #                 prev_var = f'z{i}'
 
-            elif '// hls-fpga-machine-learning insert layers' in line:
+            #     newline += indent + prev_var + '\n'
+
+            elif '// hls-fpga-machine-learning input init' in line:
                 newline = line
-                prev_var = 'x'
                 for i, layer in enumerate(layers):
-                    if layer.get_attr('write_func'):
-                        if layer.get_attr('write_weights'):
-                            newline += indent + f'let z{i} = {layer.get_attr("func_call")}({prev_var}, w{i}, b{i});\n'
-                            prev_var = f'z{i}'
+                    if layer.class_name == 'Input':
+                        newline += indent + f'default_t input[{layer.get_attr("out_dim_key")}];\n'
+                        newline += indent + f'for (int i=0; i < {layer.get_attr("out_dim_key")}; ++i) {{\n'
+                        newline += indent + indent + 'input[i] = rand() % 100;\n'
+                        newline += indent + '}\n\n'
+                    elif layer.get_attr('write_func'):
+                        newline += indent + f'default_t out{i}[{layer.get_attr("out_dim_key")}];\n'
+
+            elif '// hls-fpga-machine-learning input kernel' in line:
+                newline = ''
+                func_layers_count = 0
+                for i, layer in enumerate(layers):
+                    if layer.class_name == 'Input':
+                        newline += indent + indent + f'input, \n'
+                    elif layer.get_attr('write_func'):
+                        newline += indent + indent + f'out{i}'
+                        if func_layers_count < len([layer for layer in layers if layer.get_attr("write_func")]) - 1:
+                            newline += ',\n'
+                            func_layers_count += 1
                         else:
-                            newline += indent + f'let z{i} = {layer.get_attr("func_call")}({prev_var});\n'
-                            prev_var = f'z{i}'
-
-                newline += indent + prev_var + '\n'
-
-            elif '// hls-fpga-machine-learning top function input' in line:
-                newline = indent + f'x: {layers[0].get_attr("out_type")}[{layers[0].get_attr("out_dim_key")}]\n'
-
-            elif '// hls-fpga-machine-learning top function output' in line:
-                newline = indent + f'{layers[-1].get_attr("out_type")}[{layers[-1].get_attr("out_dim_key")}]\n'
+                            newline += '\n'
 
             elif '// hls-fpga-machine-learning load weights' in line:
                 newline = line
                 for i, layer in enumerate(layers):
                     if layer.get_attr("write_weights"):
                         # Weights
-                        newline += indent + f'let w{i} = {layer.get_attr("in_type")}[{layer.get_attr("in_dim_key")}][{layer.get_attr("out_dim_key")}]:[\n'
+                        newline += f'const default_t w{i}[{layer.get_attr("in_dim_key")}*{layer.get_attr("out_dim_key")}] = {{\n'
                         for idx_row, row in enumerate(layer.get_attr('fxp_weights')):
-                            newline += indent + indent + '['
+                            newline += indent
                             for idx_col, w in enumerate(row):
-                                newline += f'{layer.get_attr("in_type")}:{w}'
+                                newline += f'{w}'
                                 if idx_col < len(row) - 1:
                                     newline += ','
-                            newline += ']'
                             if idx_row < len(layer.get_attr("fxp_weights")) - 1:
                                     newline += ',\n'
                             else:
                                 newline += '\n'
-                        newline += indent + '];\n'
+                        newline += '};\n'
                         # Bias
-                        newline += indent + f'let b{i} = {layer.get_attr("in_type")}[{layer.get_attr("out_dim_key")}]:[\n'
-                        newline += indent + indent
+                        newline += f'const default_t b{i}[{layer.get_attr("out_dim_key")}] = {{\n'
+                        newline += indent
                         for idx_b, b in enumerate(layer.get_attr("fxp_bias")):
-                            newline += f'{layer.get_attr("in_type")}:{b}'
+                            newline += f'{b}'
                             if idx_b < len(layer.get_attr("fxp_bias")) - 1:
                                 newline += ','
-                        newline += '\n' + indent + '];\n'
-
-            elif '// hls-fpga-machine-learning call inlined weights' in line:
-                newline = indent + indent
-                weighted_layers_count = 0
-                for i, layer in enumerate(layers):
-                    if layer.class_name == 'Input':
-                        newline += 'x,'
-                    elif layer.get_attr("write_weights"):
-                        newline += f'w{i}, b{i}'
-                        if weighted_layers_count < len([layer for layer in layers if layer.get_attr("write_weights")]) - 1:
-                            newline += ', '
-                            weighted_layers_count += 1
-                newline += '\n'
+                        newline += '\n' + '};\n'
 
             # Just copy line
             else:
